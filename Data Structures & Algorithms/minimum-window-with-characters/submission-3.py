5a399ba7a2aa4c812c20bc44ad35825fe23af8e6
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-07-01 16:08:22 -0700

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,49 @@
+"""
+Brute force:
+- counter of t
+- for every substring in s:
+    - for each char in substring, minus count from t counter
+    - if t counter has none remaining -> good
+    - keep track of minimum
+
+Optimal:
+- 
+
+"""
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tCounter = Counter(t)
+        total = len(t)
+        l = 0
+        res = ""
+        resLen = float('inf')
+
+        for r in range(len(s)):
+            if s[r] in tCounter:
+                if tCounter[s[r]] > 0:
+                    total -= 1
+                tCounter[s[r]] -= 1
+
+            while total == 0:
+                if r - l + 1 < resLen:
+                    resLen = r - l + 1
+                    res = s[l:r + 1]
+
+                if s[l] in tCounter:
+                    tCounter[s[l]] += 1
+                    if tCounter[s[l]] > 0:
+                        total += 1
+
+                l += 1
+
+        return res
+
+
+
+            
+
+            
+
+            
+        
