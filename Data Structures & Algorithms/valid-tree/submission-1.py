307fff972b451c39e4c7f3d65c0af8f7142b5810
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-08-19 14:23:26 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,40 @@
+"""
+no cycles (no repeats on dfs)
+
+"""
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for t, f in edges:
+            adj[t].append(f)
+            adj[f].append(t)
+
+        self.seen = set()
+
+        def dfs(curr, prev):
+            self.seen.add(curr)
+
+            for neighbor in adj[curr]:
+                if neighbor not in self.seen:
+                    if dfs(neighbor, curr):
+                        return True
+
+                elif neighbor != prev:
+                    return True
+
+            return False
+                
+
+        self.seen = set()
+
+        if dfs(0, -1):
+            return False
+
+        for i in range(n):
+            if i not in self.seen:
+                return False
+
+
+        return True
+        
